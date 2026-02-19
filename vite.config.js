aae19609b785f122481a8a7ae85176990fff2ae0
gauravsengar912 <64774303+gauravsengar912@users.noreply.github.com>
2026-02-20 03:18:54 +0530

    Change base path in Vite config from '/lean-muscle-planner/' to '/LeanMuscles/'

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,1 @@
-import { defineConfig } from 'vite'; import react from '@vitejs/plugin-react'; export default defineConfig({ plugins:[react()], base:'/lean-muscle-planner/' });
+import { defineConfig } from 'vite'; import react from '@vitejs/plugin-react'; export default defineConfig({ plugins:[react()], base:'/LeanMuscles/' });
