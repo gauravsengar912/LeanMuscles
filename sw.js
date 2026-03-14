7d12d4df0cd6b0e4979cbd6e8acada5772d9fc51
Vimmi <vimmi05@st.com>
2026-03-14 22:18:10 +0530

    new app

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'fitai-v1';
+const CACHE = 'sweatitout-v1';
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.add('/')).catch(()=>{})); self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))); self.clients.claim(); });
 self.addEventListener('fetch', e => {
